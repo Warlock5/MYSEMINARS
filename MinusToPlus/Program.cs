9e17a92acb68de8c3328d5a0b1698c379a6e70a4
warlock <all.exe.warlock@example.com>
2022-06-24 19:51:29 +0300

    MinusToPlus

---
+++ MinusToPlus/Program.cs
@@ -0,0 +1,13 @@
+﻿int n, current;
+
+Console.Write("Input a number: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+current = n * (-1);
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
