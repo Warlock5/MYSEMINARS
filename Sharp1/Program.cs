22a66856ae03c9b5a6b2fdf071ee5b0eb90b9aff
warlock <all.exe.warlock@example.com>
2022-06-24 19:09:19 +0300

    days of the weekdays

---
+++ Sharp1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int day;
+
+Console.Write("Input number day: ");
+day = Convert.ToInt32(Console.ReadLine());
+
+if(day == 1)
+  {
+      Console.WriteLine("Today is monday");
+  }
+  if(day == 2)
+  {
+      Console.Write("Today is tuesday");
+  }
+  if(day == 3)
+  {
+      Console.Write("Today is wednesday");
+  }
+  if(day == 4)
+  {
+      Console.Write("Today is thursday");
+  }
+  if(day == 5)
+  {
+      Console.Write("Today is friday");
+  }
+  if(day == 6)
+  {
+      Console.Write("Today is saturday");
+  }
+  if(day == 7)
+  {
+      Console.Write("Today is sunday");
+  }
+  if(day > 7)
+   {
+     Console.Write("Not Found");
+   }
