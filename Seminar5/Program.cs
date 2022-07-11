05311476642ec8fe7184c7d36c051008c3bab0c5
warlock <all.exe.warlock@example.com>
2022-07-11 13:08:28 +0300

    Incorrect

---
+++ Seminar5/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задайте массив из 12 элементов, заполненный случайными числами из 
+промежутка [-9, 9]. Найдите сумму положительных элементов массива.
+*/
+/*
+int [] CreateRandomArray(int size, int minValue, int maxValue);
+{
+    int[] newArray = new int[size] ;
+
+    for(int i = 0; i < size; i +=2)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+
+
+
+int[] CreateArray( int size)
+{
+int[] newArray = new int[size];
+
+Console.WriteLine("Creating array here: ");
+for(int i = 0; i < size; i++);
+{
+    System.Console.Writ($"Input {i +1} element: ");
+    newArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine();
+return newArray;
+}
+
+
+void ShowArray(int[ array])
+{
+    for(int i =0; i < array.Lenght; i++)
+    Console.Write(ShowArray[i] + " " );
+}
+
+int FindPositiveSum(int[] array)
+if(ShowArray[i] > 0) sum += arraay[i];
+return FindPositiveSum;
+
+
+Console.Write("Input size of array:");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min posible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min posible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[ array]
+
+{
+    int sum = 0;
+
+    for(int i= 0; i < array.Length; i++);
+
+}
+*/
+/*
+Напишите программу замена элементов массива: положительные элементы 
+замените на соответствующие отрицательные, и наоборот.
+
+
+Задайте одномерный массив из 20 случайных чисел. Найдите количество 
+элементов массива, значения которых лежат в отрезке [10,99].
+*/
