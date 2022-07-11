afa79a6a9f1c2aac2b4fa73d44233abcebd28341
warlock <all.exe.warlock@example.com>
2022-07-11 12:50:55 +0300

    Seminar4 solved

---
+++ Seminar3/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Задача1.
+Написать программу которая принимает на вход номер четверти и отображает
+диапозон координат которые могут принимать точки в этой четверти.
+*/
+/*
+void ShowQuarter(int quart)
+{
+    if(quart >= 1 && quart <= 4)
+    {
+        if(quart == 1) Console.WriteLine("In this quard x > 0 and y > 0");
+        if(quart == 2) Console.WriteLine("In this quard x < 0 and y > 0");
+        if(quart == 3) Console.WriteLine("In this quard x < 0 and y < 0");
+        if(quart == 4) Console.WriteLine("In this quard x > 0 and y < 0");
+    }
+    else
+        System.Console.WriteLine("This number is uncorrect!");
+}
+
+Console.WriteLine("Input number of quard: ");
+int numQuard = Convert.ToInt32(Console.ReadLine());
+
+ShowQuarter(numQuard);
+
+
+*/
+/*____________________________________________________________________________
+Написать программу которая принимает координаты точек и
+ выдает номер четверти в которой эта точка находится.
+ */
+
+int ShowQuart(int x, int y)
+{
+    int quart = 0;
+    if (x != 0 && y != 0)
+    {
+        if (x > 0 && y > 0) quart = 1;
+        if (x > 0 && y < 0) quart = 2;
+        if (x < 0 && y < 0) quart = 3;
+        if (x < 0 && y > 0) quart = 4;
+    }
+    return quart;
+}
+Console.WriteLine("Input num x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input num y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ShowQuart(x, y));
+
+
+
+/*
+_____________________________________________________________________________
+Задача3. Написать программу которая принимает число N и возвращает
+квадраты всех чисел до N
+*/
+/*
+void Quad(int n)
+{
+    int quad;
+    int curent = 1;
+    if (n == 0) Console.Write(-1);
+    while (curent <= n)
+    {
+        quad = curent * curent;
+        Console.Write(quad + " ");
+        curent++;
+    }
+}
+
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+QuadNum(n);
+*/
---
+++ Seminar4/Program.cs
@@ -0,0 +1,86 @@
+﻿/*Напишите программу, которая принимает на вход число (А) и 
+выдаёт сумму чисел от 1 до А.
+*/
+/*
+int FindSum(int a)
+{
+    int sum = 0;
+
+    for (int curent = 1; curent <= a; curent++)
+        sum += curent;
+
+    return sum;
+}
+
+Console.WriteLine("Inputpositive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of numbers from 1 to {num} is {FindSum(num)}");
+
+/*
+____________________________________________________________________________
+Напишите программу, которая принимает на вход число и выдаёт 
+количество цифр в числе.
+*/
+/*
+int FindDigit(int a)
+{
+    int current = 0;
+
+    for (current = 0; a > 0; current++)
+    {
+        a /= 10;
+    }
+    return current;
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(FindDigit(num));
+
+/*
+___________________________________________________________________________
+Напишите программу, которая принимает на вход число N и выдаёт 
+произведение чисел от 1 до N.
+*/
+/*
+int Proizvedenie(int N)
+{
+    int sum = 0;
+    int current = 0;
+    while (current <= N)
+    {
+        sum = sum + current * current;
+        current++;
+    }
+    return sum;
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Proizvedenie(num));
+
+
+
+/*
+____________________________________________________________________________
+Напишите программу, которая принимает на вход два числа (A и B) 
+и возводит число A возведенное в натуральную степень B
+*/
+
+int Step(int a, int b)
+{
+    int sum = 1;
+    for (int current = 0; current <= b; current++)
+    {
+        sum = sum * a;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Input number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int num = Step(a, b);
+System.Console.WriteLine("Is a from dergee b " + num);
