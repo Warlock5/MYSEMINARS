df8d758ced102f79a79a75b0c6b3992a4ab30df6
warlock <all.exe.warlock@example.com>
2022-07-16 15:49:46 +0300

    Add S5

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,66 +1,113 @@
 ﻿/*
+https://gb.ru/lessons/245131
+
 Задайте массив из 12 элементов, заполненный случайными числами из 
 промежутка [-9, 9]. Найдите сумму положительных элементов массива.
 */
-/*
-int [] CreateRandomArray(int size, int minValue, int maxValue);
-{
-    int[] newArray = new int[size] ;
 
-    for(int i = 0; i < size; i +=2)
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for (int i = 0; i < size; i++)
         newArray[i] = new Random().Next(minValue, maxValue + 1);
+
     return newArray;
 }
 
-
-
-int[] CreateArray( int size)
+/*int[] CreateArray(int size)
 {
-int[] newArray = new int[size];
+    int[] newArray = new int[size];
 
-Console.WriteLine("Creating array here: ");
-for(int i = 0; i < size; i++);
+    Console.WriteLine("Creating array here: ");
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write($"Input {i + 1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    System.Console.WriteLine();
+    return newArray;
+}
+*/
+void ShowArray(int[] array)
 {
-    System.Console.Writ($"Input {i +1} element: ");
-    newArray[i] = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
 }
-System.Console.WriteLine();
-return newArray;
-}
-
-
-void ShowArray(int[ array])
+/*
+int FindePosiriveSum(int[] array)
 {
-    for(int i =0; i < array.Lenght; i++)
-    Console.Write(ShowArray[i] + " " );
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0)
+            sum += array[i];
+
+    return sum;
 }
 
-int FindPositiveSum(int[] array)
-if(ShowArray[i] > 0) sum += arraay[i];
-return FindPositiveSum;
 
-
-Console.Write("Input size of array:");
+Console.Write("Input size of array: ");
 int size = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input min posible value of elements: ");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input min posible value of elements: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
-int[ array]
 
-{
-    int sum = 0;
+/*
+int[] array = CreateRandomArray(size, min, max);
+ShowArray(array);
+int sumOfPositive = FindePosiriveSum(array);
+Console.WriteLine("Sum of positve elements in current array is" + sumOfPositive);
 
-    for(int i= 0; i < array.Length; i++);
-
-}
 */
+
 /*
 Напишите программу замена элементов массива: положительные элементы 
 замените на соответствующие отрицательные, и наоборот.
+______________________________________________________________________
+*/
+/*
+int[] InvertPositive(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+*/
 
 
+
+
+
+/*
 Задайте одномерный массив из 20 случайных чисел. Найдите количество 
 элементов массива, значения которых лежат в отрезке [10,99].
+________________________________________________________________________
 */
+int findTwoDigit(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] >= 10 && array[i] <= 99) count++;
+
+    return count;
+}
+
+
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min posible value of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min posible value of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateRandomArray(size, min, max);
+ShowArray(array);
+Console.WriteLine("Count of digit ilevtnts is " + findTwoDigit (array));
